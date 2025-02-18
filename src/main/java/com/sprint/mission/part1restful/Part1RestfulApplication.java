eade9393868e130962bc630ade427592f3e238bd
hoosa <janghoosa@gmail.com>
2025-02-18 18:46:51 +0900

    init

---
+++ src/main/java/com/sprint/mission/part1restful/Part1RestfulApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.part1restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part1RestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part1RestfulApplication.class, args);
+    }
+
+}
