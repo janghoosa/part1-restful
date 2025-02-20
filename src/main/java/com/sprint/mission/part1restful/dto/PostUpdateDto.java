2ae27fc623affb38e4e65575b8202f50edf03418
hyojkim2 <skql2548@gmail.com>
2025-02-20 17:59:54 +0900

    Dto example추가

--- src/main/java/com/sprint/mission/part1restful/dto/PostUpdateDto.java
+++ src/main/java/com/sprint/mission/part1restful/dto/PostUpdateDto.java
@@ -1,7 +1,12 @@
 package com.sprint.mission.part1restful.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record PostUpdateDto(
+        @Schema(example =  "content")
         String content,
+
+        @Schema(example = "Title")
         String title
 ) {
 }
